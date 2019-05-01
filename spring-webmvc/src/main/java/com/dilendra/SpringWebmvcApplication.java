4b87f42061d5d9c8e170ae1b30ed94153b701de8
Dilendra Prasad Pant <pant.dilendra@gmail.com>
2019-05-01 15:43:13 +0530

    Add files via upload

--- spring-webmvc/src/main/java/com/dilendra/SpringWebmvcApplication.java
+++ spring-webmvc/src/main/java/com/dilendra/SpringWebmvcApplication.java
@@ -3,7 +3,7 @@ package com.dilendra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringWebmvcApplication {
 
 	public static void main(String[] args) {
---
+++ spring-webmvc/src/main/java/com/dilendra/WebConfig.java
@@ -0,0 +1,12 @@
+package com.dilendra;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.dilendra.controller")
+public class WebConfig {
+
+}
